8d4ec3f4416f8d547fc1668b7eead1dd28ed7548
vatsarun-dev <vatsarun58@gmail.com>
2026-08-26 12:00:00 +0530

    Fix: Add missing contribution

--- client/src/app/ProtectedChat.jsx
+++ client/src/app/ProtectedChat.jsx
@@ -1,8 +1,1 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router/dom";
-const ProtectedChat = () => {
-  if (!logged) <Navigate to="/" />;
-  return <Outlet />;
-};
 
-export default ProtectedChat;
