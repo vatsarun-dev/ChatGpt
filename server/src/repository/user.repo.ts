5ee0e43d04586d667b061c8444b71f23a2c93b13
vatsarun-dev <vatsarun58@gmail.com>
2026-08-30 18:07:42 +0530

    feat(backend: setting repository )

---
+++ server/src/repository/user.repo.ts
@@ -0,0 +1,18 @@
+import { userModel } from "../models/user.model.ts";
+
+class UserRepo {
+  async createUser(input: { name: string; email: string; password: string }) {
+    const user = await userModel.create({ input });
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    await userModel.findOne({ email });
+  }
+
+  async findById(id: string) {
+    await userModel.findById(id);
+  }
+}
+
+export default UserRepo;
